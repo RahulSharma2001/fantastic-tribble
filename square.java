185771ae52dcd6a4d1a7fa217568babb3ea5fda6
RahulSharma2001 <53604829+RahulSharma2001@users.noreply.github.com>
2019-08-20 17:15:42 +0530

    Java Program to Print square of a number.

---
+++ square.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class square
+     {
+       public static void main(String[]arg)
+     {
+	int a;
+	Scanner S = new Scanner (System.in);
+	System.out.println("Enter Number For their Square");
+	
+        a = S.nextInt();
+	System.out.println(" Square="+a*a);
+
+
+     }
+     }	 
